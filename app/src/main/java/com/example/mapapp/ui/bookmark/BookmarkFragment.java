390ae9f89664d6e190d64067f57bfa37b37f7ba2
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-04-24 22:13:36 +0430

    first commit
    first commit
    
    first commit

--- app/src/main/java/com/example/mapapp/ui/bookmark/BookmarkFragment.java
+++ app/src/main/java/com/example/mapapp/ui/bookmark/BookmarkFragment.java
@@ -1,4 +1,4 @@
-package com.example.mapapp.ui.home;
+package com.example.mapapp.ui.bookmark;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,16 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mapapp.R;
 
-public class HomeFragment extends Fragment {
+public class BookmarkFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private BookmarkViewModel bookmarkViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        bookmarkViewModel = new ViewModelProvider(this).get(BookmarkViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_bookmark, container, false);
+        final TextView textView = root.findViewById(R.id.text_bookmark);
+        bookmarkViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/mapapp/ui/bookmark/BookmarkViewModel.java
+++ app/src/main/java/com/example/mapapp/ui/bookmark/BookmarkViewModel.java
@@ -1,16 +1,16 @@
-package com.example.mapapp.ui.dashboard;
+package com.example.mapapp.ui.bookmark;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class BookmarkViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public BookmarkViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is bookmark fragment");
     }
 
     public LiveData<String> getText() {
