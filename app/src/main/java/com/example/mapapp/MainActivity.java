1a320d2d792d79e124deca213050c30957de3ad4
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-04-25 23:47:15 +0430

    initial mapbox sdk

--- app/src/main/java/com/example/mapapp/MainActivity.java
+++ app/src/main/java/com/example/mapapp/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.mapapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+import com.example.mapapp.ui.bookmark.BookmarkFragment;
+import com.example.mapapp.ui.map.MapFragment;
+import com.example.mapapp.ui.setting.SettingFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,48 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
+        navView.setOnNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentManager fm = getSupportFragmentManager();
+        switch (item.getItemId()) {
+            case R.id.navigation_bookmark:
+                if (fm.findFragmentByTag("bookmark") != null) {
+                    fm.beginTransaction().show(fm.findFragmentByTag("bookmark")).commit();
+                } else {
+                    fm.beginTransaction().add(R.id.nav_host_fragment, BookmarkFragment.class, null, "bookmark").commit();
+                }
+                if (fm.findFragmentByTag("map") != null)
+                    fm.beginTransaction().hide(fm.findFragmentByTag("map")).commit();
+                if (fm.findFragmentByTag("setting") != null)
+                    fm.beginTransaction().hide(fm.findFragmentByTag("setting")).commit();
+                break;
+
+            case R.id.navigation_map:
+                if (fm.findFragmentByTag("map") != null) {
+                    fm.beginTransaction().show(fm.findFragmentByTag("map")).commit();
+                }
+                if (fm.findFragmentByTag("bookmark") != null)
+                    fm.beginTransaction().hide(fm.findFragmentByTag("bookmark")).commit();
+                if (fm.findFragmentByTag("setting") != null)
+                    fm.beginTransaction().hide(fm.findFragmentByTag("setting")).commit();
+                break;
+
+            case R.id.navigation_settings:
+                if (fm.findFragmentByTag("setting") != null) {
+                    fm.beginTransaction().show(fm.findFragmentByTag("setting")).commit();
+                } else {
+                    fm.beginTransaction().add(R.id.nav_host_fragment,  SettingFragment.class, null,"setting").commit();
+                }
+                if (fm.findFragmentByTag("map") != null)
+                    fm.beginTransaction().hide(fm.findFragmentByTag("map")).commit();
+                if (fm.findFragmentByTag("bookmark") != null)
+                    fm.beginTransaction().hide(fm.findFragmentByTag("bookmark")).commit();
+                break;
+
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/mapapp/ui/map/MapFragment.java
+++ app/src/main/java/com/example/mapapp/ui/map/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.mapapp.ui.map;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,24 +13,36 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mapapp.MainActivity;
 import com.example.mapapp.R;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
 public class MapFragment extends Fragment {
 
-    private MapViewModel mapViewModel;
+    private MapView mapView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        mapViewModel =
-                new ViewModelProvider(this).get(MapViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_map, container, false);
-        final TextView textView = root.findViewById(R.id.text_map);
-        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        getActivity().getSupportFragmentManager().beginTransaction().add(this, "map").commit();
+        Mapbox.getInstance(getContext(), getString(R.string.mapbox_access_token));
+        View fragmentlayout = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = fragmentlayout.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                    }
+                });
             }
         });
-        return root;
+        return fragmentlayout;
     }
 }
